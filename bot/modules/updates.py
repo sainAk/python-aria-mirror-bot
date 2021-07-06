94dead496bb5f044d9508517bfb69baffe6c5343
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-07-06 18:24:41 +0530

    cleanups

--- bot/modules/updates.py
+++ bot/modules/updates.py
@@ -1,83 +1,50 @@
 # Implement By https://github.com/jusidama18
 # Based on this https://github.com/DevsExpo/FridayUserbot/blob/master/plugins/updater.py
 
-import subprocess
-import sys
-from datetime import datetime
-from os import environ, execle, path, remove
+import logging
+import os
+from subprocess import CalledProcessError, run
+from sys import executable
 
-import heroku3
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
-from pyrogram import filters
+from telegram.ext.commandhandler import CommandHandler
 
-from bot import OWNER_ID, UPSTREAM_BRANCH, UPSTREAM_REPO, app
-from bot.helper import HEROKU_URL, get_text, runcmd
+from bot import UPSTREAM_BRANCH, UPSTREAM_REPO, app
+from bot.helper.ext_utils.fs_utils import clean_all
 from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.telegram_helper.filters import CustomFilters
+from bot.helper.telegram_helper.message_utils import edit_message, send_message
 
-REPO_ = UPSTREAM_REPO
-BRANCH_ = UPSTREAM_BRANCH
-
-# Update Command
+LOGGER = logging.getLogger(__name__)
 
 
-@app.on_message(filters.command(BotCommands.UpdateCommand) & filters.user(OWNER_ID))
-async def update_it(client, message):
-    msg_ = await message.reply_text("`Updating Please Wait!`")
+def update(update, context):
+    reply = send_message("Updating... Please wait!", context.bot, update)
     try:
-        repo = Repo()
-    except GitCommandError:
-        return await msg_.edit(
-            "**Invalid Git Command. Please Report This Bug To [Support Group](https://t.me/SlamMirrorSupport)**"
+        with open(".restartmsg", "w") as f:
+            f.truncate(0)
+            f.write(f"{reply.chat.id}\n{reply.message_id}\n")
+        run("rm -rf /usr/src/app/repo", shell=True, check=True)
+        edit_message("Cloning repo...", reply)
+        shell = run(
+            f"git clone --depth 1 --single-branch --branch {UPSTREAM_BRANCH} {UPSTREAM_REPO} /usr/src/app/repo",
+            shell=True,
+            check=True,
         )
-    except InvalidGitRepositoryError:
-        repo = Repo.init()
-        if "upstream" in repo.remotes:
-            origin = repo.remote("upstream")
-        else:
-            origin = repo.create_remote("upstream", REPO_)
-        origin.fetch()
-        repo.create_head(UPSTREAM_BRANCH, origin.refs.master)
-        repo.heads.master.set_tracking_branch(origin.refs.master)
-        repo.heads.master.checkout(True)
-    if repo.active_branch.name != UPSTREAM_BRANCH:
-        return await msg_.edit(
-            f"`Seems Like You Are Using Custom Branch - {repo.active_branch.name}! Please Switch To {UPSTREAM_BRANCH} To Make This Updater Function!`"
+        edit_message("Applying changes...", reply)
+        run("cp -r /usr/src/app/repo/. /usr/src/app", shell=True, check=True)
+        run("rm -rf /usr/src/app/repo/.git", shell=True, check=True)
+        run(
+            "pip install -r /usr/src/app/requirements.txt", shell=True, check=True
         )
-    try:
-        repo.create_remote("upstream", REPO_)
-    except BaseException:
-        pass
-    ups_rem = repo.remote("upstream")
-    ups_rem.fetch(UPSTREAM_BRANCH)
-    if not HEROKU_URL:
-        try:
-            ups_rem.pull(UPSTREAM_BRANCH)
-        except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
-        await runcmd("pip3 install --no-cache-dir -r requirements.txt")
-        await msg_.edit("`Updated Sucessfully! Give Me Some Time To Restart!`")
-        with open("./aria.sh", "rb") as file:
-            script = file.read()
-        subprocess.call("./aria.sh", shell=True)
-        args = [sys.executable, "-m", "bot"]
-        execle(sys.executable, *args, environ)
-        exit()
-        return
-    else:
-        await msg_.edit("`Heroku Detected! Pushing, Please wait!`")
-        ups_rem.fetch(UPSTREAM_BRANCH)
-        repo.git.reset("--hard", "FETCH_HEAD")
-        if "heroku" in repo.remotes:
-            remote = repo.remote("heroku")
-            remote.set_url(HEROKU_URL)
-        else:
-            remote = repo.create_remote("heroku", HEROKU_URL)
-        try:
-            remote.push(refspec="HEAD:refs/heads/master", force=True)
-        except BaseException as error:
-            await msg_.edit(f"**Updater Error** \nTraceBack : `{error}`")
-            return repo.__del__()
-        await msg_.edit(
-            f"`Updated Sucessfully! \n\nCheck your config with` `/{BotCommands.ConfigMenuCommand}`"
-        )
+        clean_all()
+        os.execl(executable, executable, "-m", "bot")
+    except CalledProcessError as e:
+        edit_message(f"Update failed\n <code>{e.output}</code>", reply)
+
+
+restart_handler = CommandHandler(
+    BotCommands.RestartCommand,
+    update,
+    filters=CustomFilters.owner_filter,
+    run_async=True,
+)
